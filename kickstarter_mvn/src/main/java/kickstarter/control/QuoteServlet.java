74cd8459524801ab3c16395ca556f77b705a8ec3
AVP256 <AVP256COM@GMAIL.COM>
2015-06-24 09:40:11 +0300

    attach spring-web instead spring-context

--- kickstarter_mvn/src/main/java/kickstarter/control/QuoteServlet.java
+++ kickstarter_mvn/src/main/java/kickstarter/control/QuoteServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 
 import kickstarter.control.executor.Executor;
 import kickstarter.control.executor.ExecutorImpl;
@@ -16,7 +18,8 @@ public class QuoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		WebApplicationContext context = ContextLoaderListener.getCurrentWebApplicationContext();
+		//ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		Executor executor = (Executor) context.getBean("executor");
 		// Executor executor = ExecutorImpl.getInstance();
 		executor.execute(QUOTE, request, response);
