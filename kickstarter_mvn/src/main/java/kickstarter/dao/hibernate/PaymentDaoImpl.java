d515a256083bf7a4e6418ac79132c12323697fc0
AVP256 <AVP256COM@GMAIL.COM>
2015-07-08 23:23:41 +0300

    delete @transactional in dao

--- kickstarter_mvn/src/main/java/kickstarter/dao/hibernate/PaymentDaoImpl.java
+++ kickstarter_mvn/src/main/java/kickstarter/dao/hibernate/PaymentDaoImpl.java
@@ -1,7 +1,5 @@
 package kickstarter.dao.hibernate;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import kickstarter.dao.PaymentDao;
 import kickstarter.dao.hibernate.support.DaoSupport;
 import kickstarter.entity.Payment;
@@ -14,13 +12,11 @@ public class PaymentDaoImpl implements PaymentDao {
 		this.daoSupport = daoSupport;
 	}
 
-	@Transactional
 	@Override
 	public void persist(Payment payment) throws DataBaseException {
 		daoSupport.persist(payment);
 	}
 
-	@Transactional(readOnly = true)
 	@Override
 	public Payment load(int id) throws DataBaseException {
 		return daoSupport.load(id, Payment.class);
--- kickstarter_mvn/src/main/java/kickstarter/dao/hibernate/PaymentVariantDaoImpl.java
+++ kickstarter_mvn/src/main/java/kickstarter/dao/hibernate/PaymentVariantDaoImpl.java
@@ -1,7 +1,5 @@
 package kickstarter.dao.hibernate;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import kickstarter.dao.PaymentVariantDao;
 import kickstarter.dao.hibernate.support.DaoSupport;
 import kickstarter.entity.PaymentVariant;
@@ -14,13 +12,11 @@ public class PaymentVariantDaoImpl implements PaymentVariantDao {
 		this.daoSupport = daoSupport;
 	}
 
-	@Transactional
 	@Override
 	public void persist(PaymentVariant paymentVariant) throws DataBaseException {
 		daoSupport.persist(paymentVariant);
 	}
 
-	@Transactional(readOnly = true)
 	@Override
 	public PaymentVariant load(int id) throws DataBaseException {
 		return daoSupport.load(id, PaymentVariant.class);
