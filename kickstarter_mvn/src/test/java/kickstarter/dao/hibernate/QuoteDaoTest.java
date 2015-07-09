26d6df1f0780c6a617ada5569f7b6e5a26495ace
AVP256 <AVP256COM@GMAIL.COM>
2015-07-09 14:46:57 +0300

    add QuoteDaoTest

---
+++ kickstarter_mvn/src/test/java/kickstarter/dao/hibernate/QuoteDaoTest.java
@@ -0,0 +1,50 @@
+package kickstarter.dao.hibernate;
+
+import kickstarter.dao.QuoteDao;
+import kickstarter.entity.Quote;
+import kickstarter.exception.DataBaseException;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+@ContextConfiguration(locations = { "classpath:spring-test-context.xml" })
+public class QuoteDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
+	@Autowired
+	private QuoteDao quoteDao;
+
+	@Test
+	@Rollback(true)
+	public void persistTest() throws DataBaseException {
+		Quote quote = new Quote();
+		quote.setQuote("quoteTest");
+
+		quoteDao.persist(quote);
+		int id = quote.getId();
+
+		Quote result = quoteDao.load(id);
+		assertEquals("quoteTest", result.getQuote());
+	}
+
+	@Test
+	@Rollback(true)
+	public void loadRandomTest() throws DataBaseException {
+		Quote result = quoteDao.loadRandom();
+		assertNotNull(result);
+	}
+
+	@Test(expected = DataBaseException.class)
+	@Rollback(true)
+	public void shouldThrowException_whenPersistNull() throws DataBaseException {
+		quoteDao.persist(null);
+	}
+
+	@Test(expected = DataBaseException.class)
+	@Rollback(true)
+	public void shouldThrowException_whenIncorrectId() throws DataBaseException {
+		quoteDao.load(-1);
+	}
+}
