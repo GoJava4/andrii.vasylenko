056e5048443af36ad51ea458e999b86f3d7ec843
AVP256 <AVP256COM@GMAIL.COM>
2015-07-17 15:41:55 +0300

    add Projects.xhtml... this branch will migrate to a new project 'kickstarter_client'

--- kickstarter_mvn/src/main/java/kickstarter/entity/Project.java
+++ kickstarter_mvn/src/main/java/kickstarter/entity/Project.java
@@ -20,7 +20,7 @@ public class Project implements Serializable {
 	private Set<Payment> payments;
 	private Set<Question> questions;
 	private Set<PaymentVariant> paymentVariants;
-	private int collectAmount;
+	private Integer collectAmount;
 
 	public int getDaysLeft() {
 		return Days.daysBetween(DateTime.now(), getFinalDate()).getDays();
@@ -114,11 +114,11 @@ public class Project implements Serializable {
 		this.paymentVariants = paymentVariants;
 	}
 
-	public int getCollectAmount() {
+	public Integer getCollectAmount() {
 		return collectAmount;
 	}
 
-	public void setCollectAmount(int collectAmount) {
+	public void setCollectAmount(Integer collectAmount) {
 		this.collectAmount = collectAmount;
 	}
 }
