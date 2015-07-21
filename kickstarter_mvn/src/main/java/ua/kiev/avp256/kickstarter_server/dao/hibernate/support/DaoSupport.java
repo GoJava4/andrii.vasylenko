2ee5393fe745b0205c66424afc1e9147e15338e2
AVP256 <AVP256COM@GMAIL.COM>
2015-07-21 09:57:16 +0300

    add comments for methods of 'DaoSupport' interface

--- kickstarter_mvn/src/main/java/ua/kiev/avp256/kickstarter_server/dao/hibernate/support/DaoSupport.java
+++ kickstarter_mvn/src/main/java/ua/kiev/avp256/kickstarter_server/dao/hibernate/support/DaoSupport.java
@@ -5,11 +5,44 @@ import java.util.List;
 import org.hibernate.Session;
 
 public interface DaoSupport {
+	/**
+	 * Obtains the current session.
+	 */
 	Session getCurrentSession();
 
+	/**
+	 * Persists specified entity.
+	 *
+	 * @param entity
+	 *            - ENTITY instance to persist.
+	 * 
+	 * @throws DataNotFoundException
+	 *             when entity is null
+	 */
 	void persist(Object entity);
 
+	/**
+	 * Returns entity with specified Id.
+	 *
+	 * @param id
+	 *            - id of the entity to return.
+	 * @param clazz
+	 *            - type of the entity.
+	 * 
+	 * @throws DataNotFoundException
+	 *             when there are no entity with current 'id' in DB
+	 */
 	<ENTITY> ENTITY load(int id, Class<ENTITY> clazz);
 
+	/**
+	 * Check result of loading entities list. It's should be not null and not
+	 * empty
+	 *
+	 * @param result
+	 *            - list of entities
+	 * 
+	 * @throws DataNotFoundException
+	 *             when result is null or result is empty
+	 */
 	void check(List<?> result);
 }
